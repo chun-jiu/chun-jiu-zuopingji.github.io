f7bf8178ba33aa1fd6977b11a6f0f5b6cceb116a
chun-jiu <zhonronghao@gmail.com>
2026-06-11 11:06:45 +0800

    壁纸桌面端改为加载原始PNG无损画质

--- js/main.js
+++ js/main.js
@@ -172,9 +172,9 @@ function createItemHTML(work, idx) {
   if (hasVideo) {
     imgTag = '<video src="images/' + work.video + '" autoplay loop muted playsinline alt="' + work.title + '" preload="metadata"></video>';
   } else if (work.wallpaper) {
-    // 桌面端直接显示原图，移动端加载缩略图
+    // 桌面端加载原始PNG无损画质，移动端加载WebP缩略图
     imgTag = '<picture>' +
-      '<source srcset="' + getImgPath(work) + '" media="(min-width: 769px)">' +
+      '<source srcset="' + getImgPath(work).replace('.webp', '.png') + '" media="(min-width: 769px)">' +
       '<source srcset="images/thumbnails-lg/' + work.file + '" media="(min-width: 481px)">' +
       '<img src="' + getThumbPath(work) + '" alt="' + work.title + '" loading="lazy" decoding="async">' +
       '</picture>';
